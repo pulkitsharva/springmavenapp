8f79b30c089865fb683ba3a06477642dffff01ec
pulkitsharva <pulkit.sharva@gmail.com>
2014-09-09 12:53:49 +0530

    data saved in db

--- src/main/java/com/akosha/spring/service/PersonService.java
+++ src/main/java/com/akosha/spring/service/PersonService.java
@@ -3,30 +3,37 @@ package com.akosha.spring.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.akosha.spring.dao.PersonDAO;
 import com.akosha.spring.entity.Person;
 
+@Service
 public class PersonService 
 {
 	@Autowired
-	PersonDAO personDAO;
+	private PersonDAO personDAO;
 	
+	@Transactional
 	public Person getParticularPerson(int id)
 	{
 		return personDAO.getParticularPerson(id);
 	}
 	
+	@Transactional
 	public List<Person> listPerson()
 	{
 		return personDAO.listPerson();
 	}
 	
+	@Transactional
 	public void updatePersonDetails(int id, String name)
 	{
 		personDAO.updatePersonDetails(id, name);
 	}
 	
+	@Transactional
 	public void save(Person person)
 	{
 		personDAO.save(person);
